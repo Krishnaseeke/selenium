7b8c42456bc0062d8d56ae81cb01cd21364477d7
Krishna <seekegopi@gmail.com>
2023-03-06 06:21:26 +0530

    Using chropath extension and thread sleep able to run the script on basis of locators.

--- LocatorsConcept.java
+++ LocatorsConcept.java
@@ -3,11 +3,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatorsConcept {
-    public static void main(String [] args){
+    public static void main(String [] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","/Users/seekekrishna/eclipse-workspace/Microsite_V1/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.embibe.com/questions/The-functional-unit-of-lung-is/EM2853418");
-        //driver.findElement(By.linkText("/html/body/div[1]/div/div[1]/div/div/div/button")).click();
-        driver.findElement(By.cssSelector("ecSxel")).click();
+        //driver.get("https://www.embibe.com/login");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[@class='sc-gswNZR ecSxel']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//input[@class='sc-kDvujY iXiPEQ']")).click();
+        driver.findElement(By.xpath("//input[@class='sc-kDvujY iXiPEQ']")).sendKeys("6303960573");
+        driver.findElement(By.xpath("//button[@class='sc-gswNZR cAXbMq']")).click();
     }
 }
